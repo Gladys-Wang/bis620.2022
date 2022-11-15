e215eadef303db85450eec966b02117356220bac
Gladys-Wang <chengxi.wang@yale.edu>
2022-11-15 10:51:23 -0500

    Added Commit 11/15.

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -16,11 +16,75 @@ test_that(
 )
 
 test_that(
+  "The accel_plot() returns a ggplot object when there's a column named freq",
+  {
+    freq <- c(1.1, 1.2, 1.3)
+    X <- c(0.672, -0.844, -0.891)
+    Y <- c(0.672, -0.844, -0.891)
+    Z <- c(0.672, -0.844, -0.891)
+    df <- data.frame(freq, X, Y, Z)
+    p = accel_plot(df)
+    expect_true(inherits(p, "gg"))
+  }
+)
+
+test_that(
   "The accel_plot() is correct for time-series data.",
   {
     data(ukb_accel)
     p = accel_plot(ukb_accel[1:100,])
     #+ ggplot2::theme_minimal()
-    expect_doppelganger("first-100-samples", p)
+    vdiffr::expect_doppelganger("first-100-samples", p)
+  }
+)
+
+test_that(
+  "The spectral_signature() works",
+  {
+    data(ukb_accel)
+    df = spectral_signature(ukb_accel[1:100,])
+    expect_snapshot(df)
+  }
+)
+
+test_that(
+  "The spectral_signature() works",
+  {
+    data(ukb_accel)
+    df = spectral_signature(ukb_accel[1:100,])
+    expect_snapshot(df)
+  }
+)
+
+test_that(
+  "The spectral_signature() works when take_log is TRUE",
+  {
+    data(ukb_accel)
+    df = spectral_signature(ukb_accel[1:100,], take_log = TRUE)
+    expect_snapshot(df)
+  }
+)
+
+test_that(
+  "The hello() works",
+  {
+    ch = hello()
+    expect_snapshot(ch)
+  }
+)
+
+test_that(
+  "The hello() works when name is not null",
+  {
+    ch = hello(name = "Gladys")
+    expect_snapshot(ch)
+  }
+)
+
+test_that(
+  "The hello() works if invisible",
+  {
+    ch = hello(invisible = TRUE)
+    expect_snapshot(ch)
   }
 )
