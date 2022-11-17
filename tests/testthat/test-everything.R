f3efea68c51f0b0cec4ae39c8c621f6b97a5a752
Gladys-Wang <chengxi.wang@yale.edu>
2022-11-17 10:43:49 -0500

    Added Commit 11/17

--- tests/testthat/test-everything.R
+++ tests/testthat/test-everything.R
@@ -18,13 +18,13 @@ test_that(
 test_that(
   "The accel_plot() returns a ggplot object when there's a column named freq",
   {
-    freq <- c(1.1, 1.2, 1.3)
-    X <- c(0.672, -0.844, -0.891)
-    Y <- c(0.672, -0.844, -0.891)
-    Z <- c(0.672, -0.844, -0.891)
-    df <- data.frame(freq, X, Y, Z)
-    p = accel_plot(df)
-    expect_true(inherits(p, "gg"))
+    #freq <- c(1.1, 1.2, 1.3)
+    #X <- c(0.672, -0.844, -0.891)
+    #Y <- c(0.672, -0.844, -0.891)
+    #Z <- c(0.672, -0.844, -0.891)
+    #df <- data.frame(freq, X, Y, Z)
+    p = accel_plot(spectral_signature(ukb_accel[1:100,]))
+    vdiffr::expect_doppelganger("freq", p)
   }
 )
 
